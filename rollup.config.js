73cd2513ea9c247b08921fa96696c521a37ee3ed
Sebastian Schwarz <12232063+itsmebasti@users.noreply.github.com>
2022-09-11 12:27:04 +0200

    Moved project to new lwc rollup solution

---
+++ rollup.config.js
@@ -0,0 +1,42 @@
+import lwc from '@lwc/rollup-plugin';
+import json from '@rollup/plugin-json';
+import replace from '@rollup/plugin-replace';
+import { nodeResolve as resolveSubdirectoryImports } from '@rollup/plugin-node-resolve';
+import copy from 'rollup-plugin-copy';
+import serve from 'rollup-plugin-serve';
+import { terser } from 'rollup-plugin-terser';
+import livereload from 'rollup-plugin-livereload';
+import del from 'rollup-plugin-delete';
+
+
+const production = (process.env.NODE_ENV === 'production');
+const watching = Boolean(process.env.ROLLUP_WATCH);
+
+export default {
+    input: 'src/index.js',
+    output: {
+        sourcemap: watching,
+        dir: 'dist',
+        format: 'esm',
+    },
+    watch: {
+        clearScreen: false,
+        include: 'src/**'
+    },
+    plugins: [
+        del({ targets: 'dist/*', runOnce: true }),
+        resolveSubdirectoryImports(),
+        lwc({ sourcemap: watching, exclude: '**/*.json' }),
+        json(),
+        replace({ preventAssignment: true, 'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV) }),
+        copy({
+            targets: [
+                { src: 'src/index.html', dest: 'dist' },
+                { src: 'src/assets', dest: 'dist' },
+            ]
+        }),
+        production && terser(),
+        watching && serve('dist'),
+        watching && livereload({ watch: 'dist', delay: 400 })
+    ].filter(Boolean)
+};
