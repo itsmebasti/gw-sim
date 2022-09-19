0a138af5262e056f867e0e4e086c3cd03bb8fbf5
Sebastian Schwarz <12232063+itsmebasti@users.noreply.github.com>
2022-09-19 20:32:10 +0200

    added configuration tab and defaulted darkmode

--- src/modules/gw/app/app.js
+++ src/modules/gw/app/app.js
@@ -1,5 +1,24 @@
 import { LightningElement } from 'lwc';
+import { CacheMixin } from '../../../classes/framwork/cache/cache';
 
-export default class App extends LightningElement {
-    
+export default class App extends CacheMixin(LightningElement) {
+
+    cache = this.cached({
+        darkmode: true
+    });
+
+    connectedCallback() {
+        this.cache.darkmode && this.setAttribute('dark-mode', 'dark-mode');
+    }
+
+    toggleTheme() {
+        if(this.getAttribute('dark-mode')) {
+            this.cache.darkmode = false;
+            this.removeAttribute('dark-mode');
+        }
+        else {
+            this.cache.darkmode = true;
+            this.setAttribute('dark-mode', 'dark-mode');
+        }
+    }
 }
